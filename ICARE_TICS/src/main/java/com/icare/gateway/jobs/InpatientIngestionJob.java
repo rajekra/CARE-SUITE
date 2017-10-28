215df56e42a39b180cf439d5e2068d74338d1c06
rajekra <krajasekaranmca@gmail.com>
2017-10-27 22:04:18 -0400

    2
    2

--- ICARE_TICS/src/main/java/com/icare/gateway/jobs/InpatientIngestionJob.java
+++ ICARE_TICS/src/main/java/com/icare/gateway/jobs/InpatientIngestionJob.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.spark.launcher.SparkAppHandle;
 import org.apache.spark.launcher.SparkLauncher;
 import org.springframework.scheduling.annotation.Async;
@@ -12,18 +14,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class InpatientIngestionJob {
 
-	@Async
-	public void la() throws IOException, InterruptedException
+	@PostConstruct
+	public void init() throws IOException, InterruptedException
 	{
-		SparkLauncher sparkLauncher = new SparkLauncher()
-         .setAppResource("/my/app.jar")
-         .setMainClass("my.spark.app.Main")
-         .setMaster("local")
-         .setConf(SparkLauncher.DRIVER_MEMORY, "2g");
+		launchJob(); 
+	}
+	@Async
+	public void launchJob() throws IOException, InterruptedException
+	{
+		SparkLauncher sparkLauncher = new SparkLauncher();
+		sparkLauncher.setSparkHome("/usr/local/spark-1.4.0")
+				.addJar("/usr/local/spark-1.4.0/executable_jars/hbeat-ml-jobs.jar")
+				.addJar("/home/rajekra/spark_1.4_JARS/spark-streaming-kafka-assembly_2.10-1.4.0.jar")
+				.setAppResource("/usr/local/spark-1.4.0/executable_jars/hbeat-ml-jobs.jar")
+				.setMainClass("ConsumeInpatientClaim")
+				.setMaster("yarn")
+				.setDeployMode("cluster")
+				.setAppName("ConsumeInpatientClaim");
 		
 		final CountDownLatch countDownLatch = new CountDownLatch(1);
 		SparkAppListener sparkAppListener = new SparkAppListener(countDownLatch);
 		SparkAppHandle appHandle = sparkLauncher.startApplication(sparkAppListener);
+	//	sparkLauncher.addAppArgs("");
 		Thread sparkAppListenerThread = new Thread(sparkAppListener);
 		sparkAppListenerThread.start();
 		long timeout = 120;
